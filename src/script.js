05938867fa6fbc0cab64f409c05931d83baa6b4b
João Schweitzer <142324868+J-Schweitzer@users.noreply.github.com>
2026-03-03 20:31:43 -0300

    Remove empty line between player definitions and logElement

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 const player1 = { NOME: "Mario", VELOCIDADE: 4, MANOBRABILIDADE: 3, PODER: 3, PONTOS: 0 };
 const player2 = { NOME: "Luigi", VELOCIDADE: 3, MANOBRABILIDADE: 4, PODER: 4, PONTOS: 0 };
 
-
 const logElement = document.getElementById("log");
 
 const delay = (ms) => new Promise(res => setTimeout(res, ms));
